8d6f566be48cb1f173ee37e7963fb91da64ca14d
Abdullah149081 <masudmd425@gmail.com>
2023-05-07 21:06:32 +0600

    get specific user api

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
     // important POST C
     app.post("/users", async (req, res) => {
       const user = req.body;
