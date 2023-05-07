0d7fef8dc2c099161c723be376deb71451fae4b0
Abdullah149081 <masudmd425@gmail.com>
2023-05-07 18:50:29 +0600

    get mongodb

--- index.js
+++ index.js
@@ -33,13 +33,18 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    //
+    // important
     const database = client.db("userDB");
     const userCollection = database.collection("user");
-
+    //important
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // important
     app.post("/users", async (req, res) => {
       const user = req.body;
-      console.log("new user", user);
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
